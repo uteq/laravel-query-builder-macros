b91fdeb960d7425017043b5ac5b928e9e845aa06
Nathan Jansen <info@nathanjansen.nl>
2023-03-26 10:01:56 +0200

    Removed ray from raw query

--- src/Macros/RawQuery.php
+++ src/Macros/RawQuery.php
@@ -7,16 +7,12 @@ class RawQuery
     public function __invoke(): \Closure
     {
         return function () {
-            $string = vsprintf(
+            return vsprintf(
                 format: str_replace('?', '%s', $this->toSql()),
                 values: collect($this->getBindings())
                     ->map(fn ($binding) => is_numeric($binding) ? $binding : "'{$binding}'")
                     ->toArray()
             );
-
-            ray($string);
-
-            return $string;
         };
     }
 }
